1ae426b95bf2e200d5fbe9ab9e7d6323ed987e8a
GitHub Action <action@github.com>
2025-01-27 23:53:44 +0000

    Initialize template repository proper
    Set package name to com.github.emmanuelbruno493.lobaaddons
    Set project name to LobaAddons
    Set modid to lobaaddons

--- src/main/java/com/github/emmanuelbruno493/lobaaddons/ExampleMod.java
+++ src/main/java/com/github/emmanuelbruno493/lobaaddons/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.emmanuelbruno493.lobaaddons;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "lobaaddons", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
